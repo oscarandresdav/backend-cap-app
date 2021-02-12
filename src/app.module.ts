7dc6ab948d493a1cb6a5de7ef97e87ad500ac4f4
Oscar Davila <oscar7822002@gmail.com>
2021-02-12 16:45:19 -0500

    Heroku db

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,15 +19,13 @@ import { CatalogModule } from './catalog/catalog.module';
       password: 'pass123', // user password
       database: 'postgres', // name of our database, 
       */
+     // mysql://b5aaab334b3cc9:6c3803c5@us-cdbr-east-03.cleardb.com/heroku_86bafe97a000739?reconnect=true
       type: 'mysql',
-      host: 'localhost',
+      host: 'us-cdbr-east-03.cleardb.com',
       port: 3306,
-      username: 'user',
-      password: 'pass123',
-      database: 'db',
-      // username: 'compqkch_cap',
-      // password: 'Eddidi123.',
-      // database: 'compqkch_cap',
+      username: 'b5aaab334b3cc9',
+      password: '6c3803c5',
+      database: 'heroku_86bafe97a000739',
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
     }),
