e4ca3830d823788a2086e7e838da65e5d20a502c
Oscar Davila <oscar7822002@gmail.com>
2021-02-12 18:04:25 -0500

    Entorno de desarrollo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,26 +7,15 @@ import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CatalogModule,
     TypeOrmModule.forRoot({
-      /* 
-      ================
-      === POSTGRES ===
-      ================ 
-      type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'postgres', // name of our database, 
-      */
-     // mysql://b5aaab334b3cc9:6c3803c5@us-cdbr-east-03.cleardb.com/heroku_86bafe97a000739?reconnect=true
       type: 'mysql',
-      host: 'us-cdbr-east-03.cleardb.com',
-      port: 3306,
-      username: 'b5aaab334b3cc9',
-      password: '6c3803c5',
-      database: 'heroku_86bafe97a000739',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
       synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
     }),
